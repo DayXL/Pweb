48dae0b31f91145ab9089f8a9e61fac847933bf0
DayXL <day.luc.xav0606@gmail.com>
2023-11-11 11:17:20 -0300

    começando receita 3

---
+++ Receita3/pages/index.js
@@ -0,0 +1,36 @@
+export default function Movies({data}){
+
+    return (
+
+        <div>
+
+            <div>
+
+            {data.Search.map( (m) => <div>{m.Title} --- {m.Year}</div>  )}               
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export async function getServerSideProps(context){
+
+    const res = await fetch(`http://www.omdbapi.com/?apikey=e6f69885&s=bagdad`)
+  
+    const data = await res.json()
+  
+    return {
+  
+        props: {
+  
+            data
+  
+        }
+  
+    }
+  
+}
+
