38f72c55404b25148c7fa98f42c09f57fdf90a53
DayXL <day.luc.xav0606@gmail.com>
2023-11-11 11:45:04 -0300

    feito questão 2

--- Receita3/pages/index.js
+++ Receita3/pages/index.js
@@ -6,7 +6,11 @@ export default function Movies({data}){
 
             <div>
 
-            {data.Search.map( (m) => <div>{m.Title} --- {m.Year}</div>  )}               
+            {data.Search.map( (m) => <div>{m.Title} --- {m.Year} 
+                <br/>
+                <img src={m.Poster} alt={m.Title} style={{ width: '100px' }} />
+                
+            </div>  )}               
 
             </div>
 
@@ -18,7 +22,7 @@ export default function Movies({data}){
 
 export async function getServerSideProps(context){
 
-    const res = await fetch(`http://www.omdbapi.com/?apikey=e6f69885&s=bagdad`)
+    const res = await fetch(`http://www.omdbapi.com/?apikey=e6f69885&s=bagdad&y=1953`)
   
     const data = await res.json()
   
